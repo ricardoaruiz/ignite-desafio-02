ee27d152fbbc6049b53507dac7564de82622c2f8
Ricardo Ruiz <ricardo.ruiz.ext@dasa.com.br>
2021-06-28 19:48:56 -0300

    feat: Solution

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,35 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from "./MovieCard";
+
+type Rating = {
+  Source: string;
+  Value: string;
 }
+
+export type Movie = {
+  imdbID: string
+  Title: string
+  Poster: string
+  Ratings: Rating[]
+  Runtime: string;
+}
+
+type ContentProps = {
+  genre: string
+  movies: Movie[]
+}
+
+export const Content = ({ genre, movies }: ContentProps)  => (
+  <div className="container">
+      <header>
+        <span className="category">Categoria:<span> {genre}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+)
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,32 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from "./Button"
+
+type Genre = {
+  id: number
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family'
+  title: string
 }
+
+type SideBarProps = {
+  genres: Genre[]
+  selectedGenreId: number
+  onItemMenuClick: (genreId: number) => void
+}
+
+export const SideBar = ({ genres, selectedGenreId, onItemMenuClick }: SideBarProps) => (
+  <nav className="sidebar">
+    <span>Watch<p>Me</p></span>
+
+    <div className="buttons-container">
+      {genres.map(genre => (
+        <Button
+          key={String(genre.id)}
+          title={genre.title}
+          iconName={genre.name}
+          onClick={() => onItemMenuClick(genre.id)}
+          selected={selectedGenreId === genre.id}
+        />
+      ))}
+    </div>
+
+  </nav>
+)
